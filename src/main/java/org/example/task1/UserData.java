9e9fecc399db585a5dd65f9f7d73cc79b2b25f95
Olga <olga-ork@yandex.ru>
2024-01-21 19:38:01 +0300

    Seminar3

---
+++ src/main/java/org/example/task1/UserData.java
@@ -0,0 +1,26 @@
+package org.example.task1;
+import java.io.Serializable;
+
+public class UserData implements Serializable {
+    public UserData(String name, int age, String password) {
+        this.name = name;
+        this.age = age;
+        this.password = password;
+    }
+
+    private String name;
+    private int age;
+    transient String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
